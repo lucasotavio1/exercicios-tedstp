29ce0b640beeedcdf08df1070f438a53f2145988
Lucas Otávio <lucasotavioaf@gmail.com>
2024-11-20 02:19:10 -0300

    Add files via upload

---
+++ beecrowd/1005/1005.py
@@ -0,0 +1,6 @@
+a = float(input())
+b = float(input())
+peso_a = 3.5
+peso_b = 7.5
+media = ((a * peso_b) + (b * peso_b)) / (peso_a + peso_b)
+print('MEDIA = {:.5f}'.format(media))
---
+++ beecrowd/1012/1012.py
@@ -0,0 +1,12 @@
+a, b, c = map(float, input().split())
+pi = 3.14159
+area_tri = a * c/2
+area_cir = pi * c ** 2
+area_trap = (a + b) * c/2
+area_qua = b ** 2
+area_ret = a * b
+print('TRIANGULO: {:.3f}'.format(area_tri))
+print('CIRCULO: {:.3f}'.format(area_cir))
+print('TRAPEZIO: {:.3f}'.format(area_trap))
+print('QUADRADO: {:.3f}'.format(area_qua))
+print('RETANGULO: {:.3f}'.format(area_ret))
