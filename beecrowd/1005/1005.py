f0c12a16a8d1dd7b877141671b7550ba70c49e7d
lucasotavio1 <lucasotavioaf@gmail.com>
2024-12-20 00:32:39 -0300

    1005

--- beecrowd/1005/1005.py
+++ beecrowd/1005/1005.py
@@ -2,5 +2,5 @@ a = float(input())
 b = float(input())
 peso_a = 3.5
 peso_b = 7.5
-media = ((a * peso_b) + (b * peso_b)) / (peso_a + peso_b)
+media = ((a * peso_a) + (b * peso_b)) / (peso_a + peso_b)
 print('MEDIA = {:.5f}'.format(media))
