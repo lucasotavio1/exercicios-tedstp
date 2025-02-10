b4009c91331eb1c657e911382e62f9125a1c099c
lucasotavio1 <lucasotavioaf@gmail.com>
2025-02-10 18:14:38 -0300

    1116

---
+++ beecrowd/1116/1116.py
@@ -0,0 +1,9 @@
+n = int(input())
+for _ in range(n):
+    x, y = map(int, input().split())
+    try:
+        x / y
+    except ZeroDivisionError:
+        print('divisao impossivel')
+    else:
+        print(x/y)
