e60b826f9e0d473aeb3d5d32cb54ded75e614b49
lucasotavio <lucasotavioaf@gmail.com>
2025-02-11 19:46:06 -0300

    1133

--- beecrowd/1133/1133.py
+++ beecrowd/1133/1133.py
@@ -1,8 +1,8 @@
 x = int(input())
 y = int(input())
-inicio = min(x, y)
-fim = max(x, y)
 
-for resultado in range(inicio, fim):
+if x > y:
+    x, y = y, x
+for resultado in range(x + 1, y):
     if resultado % 5 == 2 or resultado % 5 == 3:
         print(resultado)
